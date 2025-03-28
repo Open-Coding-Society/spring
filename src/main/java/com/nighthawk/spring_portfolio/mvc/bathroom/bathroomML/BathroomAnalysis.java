045b2f5ddedc8e10cc5edd1c04efcaa6ca2ad984
Th35py27 <nvallamkondu27@gmail.com>
2025-03-28 11:39:53 -0700

    changes to analysis

--- src/main/java/com/nighthawk/spring_portfolio/mvc/bathroom/bathroomML/BathroomAnalysis.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/bathroom/bathroomML/BathroomAnalysis.java
@@ -2,7 +2,9 @@ package com.nighthawk.spring_portfolio.mvc.bathroom.bathroomML;
 
 import tech.tablesaw.api.Table;
 import tech.tablesaw.api.BooleanColumn;
+import tech.tablesaw.api.DoubleColumn;
 import tech.tablesaw.api.NumericColumn;
+import tech.tablesaw.api.StringColumn;
 import tech.tablesaw.plotly.Plot;
 import tech.tablesaw.plotly.api.Histogram;
 import tech.tablesaw.plotly.api.VerticalBarPlot;
@@ -19,6 +21,14 @@ public class BathroomAnalysis {
             throw new IllegalArgumentException("File not found: bathroom_cleaned.csv");
         }
         Table bathroom_info = Table.read().csv(inputStream);
+
+        StringColumn nameCol = StringColumn.create("Name");
+        DoubleColumn durationCol = DoubleColumn.create("Duration"); // in minutes
+        DoubleColumn durationByPeriodCol = DoubleColumn.create("Average Duration By Period"); // in minutes
+        StringColumn dateCol = StringColumn.create("Date");
+        BooleanColumn abnormalCol = BooleanColumn.create("Abnormal");
+
+        
 
     }
 }
