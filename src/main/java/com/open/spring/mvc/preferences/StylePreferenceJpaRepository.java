6669ca38294fd657de6db28da1204446778b9338
kush1434 <kushkshah3@gmail.com>
2025-12-14 20:21:18 -0800

    style_preference table removal

--- src/main/java/com/open/spring/mvc/preferences/StylePreferenceJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.open.spring.mvc.preferences;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface StylePreferenceJpaRepository extends JpaRepository<StylePreference, Long> {
-    Optional<StylePreference> findByPersonId(Long personId);
-}
