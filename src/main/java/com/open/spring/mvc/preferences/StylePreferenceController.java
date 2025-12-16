0e82dac8708a74fb44768b4acd55fe5044f857ca
kush1434 <kushkshah3@gmail.com>
2025-12-16 11:52:11 -0800

    /id replacement with /uid

--- src/main/java/com/open/spring/mvc/preferences/StylePreferenceController.java
+++ src/main/java/com/open/spring/mvc/preferences/StylePreferenceController.java
@@ -1,5 +1,8 @@
 package com.open.spring.mvc.preferences;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import com.open.spring.mvc.person.Person;
 import com.open.spring.mvc.person.PersonJpaRepository;
 
-import java.util.Map;
-
 @RestController
 @RequestMapping("/api/styles")
 public class StylePreferenceController {
@@ -17,32 +18,23 @@ public class StylePreferenceController {
     @Autowired
     private PersonJpaRepository personRepository;
 
-    @GetMapping("/person/{personId}")
-    public ResponseEntity<Map<String, Object>> getStyleByPersonId(@PathVariable long personId) {
-        var optional = personRepository.findById(personId);
-        if (optional.isPresent()) {
-            return new ResponseEntity<>(optional.get().getStyleData(), HttpStatus.OK);
+    @GetMapping("/person/{uid}")
+    public ResponseEntity<Map<String, Object>> getStyleByUid(@PathVariable String uid) {
+        Person person = personRepository.findByUid(uid);
+        if (person != null) {
+            return new ResponseEntity<>(person.getStyleData(), HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping("/person/{personId}")
-    public ResponseEntity<Map<String, Object>> createOrUpdateStyle(@PathVariable long personId, @RequestBody Map<String, Object> styleData) {
-        var optional = personRepository.findById(personId);
-        if (optional.isPresent()) {
-            Person person = optional.get();
-            person.setStyleData(styleData);
-            personRepository.save(person);
-            return new ResponseEntity<>(person.getStyleData(), HttpStatus.CREATED);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
 
-    @PutMapping("/person/{personId}")
-    public ResponseEntity<Map<String, Object>> updateStyleByPersonId(@PathVariable long personId, @RequestBody Map<String, Object> styleData) {
-        var optional = personRepository.findById(personId);
-        if (optional.isPresent()) {
-            Person person = optional.get();
+    @PostMapping("/person/{uid}")
+    public ResponseEntity<Map<String, Object>> createOrUpdateStyleByUid(
+            @PathVariable String uid,
+            @RequestBody Map<String, Object> styleData) {
+
+        Person person = personRepository.findByUid(uid);
+        if (person != null) {
             person.setStyleData(styleData);
             personRepository.save(person);
             return new ResponseEntity<>(person.getStyleData(), HttpStatus.OK);
@@ -50,14 +42,29 @@ public class StylePreferenceController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @DeleteMapping("/person/{personId}")
-    public ResponseEntity<Void> deleteStyleByPersonId(@PathVariable long personId) {
-        var optional = personRepository.findById(personId);
-        if (optional.isPresent()) {
-            Person person = optional.get();
-            person.setStyleData(new java.util.HashMap<>());
+
+    @PutMapping("/person/{uid}")
+    public ResponseEntity<Map<String, Object>> updateStyleByUid(
+            @PathVariable String uid,
+            @RequestBody Map<String, Object> styleData) {
+
+        Person person = personRepository.findByUid(uid);
+        if (person != null) {
+            person.setStyleData(styleData);
+            personRepository.save(person);
+            return new ResponseEntity<>(person.getStyleData(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/person/{uid}")
+    public ResponseEntity<Void> deleteStyleByUid(@PathVariable String uid) {
+        Person person = personRepository.findByUid(uid);
+        if (person != null) {
+            person.setStyleData(new HashMap<>());
             personRepository.save(person);
             return new ResponseEntity<>(HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }}
+    }
+}
